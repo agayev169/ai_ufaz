5d34e578054209041d7538b2c454720b7b5c9b4c
agayev169 <agayev169@gmail.com>
2019-10-05 17:47:33 +0400

    Add str_to_one_hot function and amend float_to_one_hot function

--- iris/decision_tree/tree.py
+++ iris/decision_tree/tree.py
@@ -18,17 +18,44 @@ class Node():
 
 
 def float_to_one_hot(df, cols="all", classes_n=3):
+    """
+    Makes float data one-hot
+
+    Args:
+        df: Pandas Dataframe obejct
+        cols: "all" or list of name of columns
+        classes_n: number of classes to have in resulting one-hot Dataframe
+    """
+    if cols != "all" and not isinstance(cols, list):
+        raise AssertionError("cols must be either \"all\" or a list of names of columns")
     if cols == "all":
         cols = [col for col in df.columns]
+    df = df.copy()
     for col in cols:
         for cl in range(classes_n):
+            df = df.sort_values(by=col)
             df.iloc[:][col][cl * len(df[col]) // 3 : (cl + 1) * len(df[col]) // 3] = cl
-    return df
+    return df.sort_index()
 
 
-def str_to_one_hot(df, cols="class"):
-    # TODO: implement!
-    pass
+def str_to_one_hot(df, cols=["class"]):
+    """
+    Makes string data one-hot
+
+    Args:
+        df: Pandas Dataframe obejct
+        cols: "all" or list of name of columns
+    """
+    if cols != "all" and not isinstance(cols, list):
+        raise AssertionError("cols must be either \"all\" or a list of names of columns")
+    if cols == "all":
+        cols = [col for col in df.columns]
+    df = df.copy()
+    dictionary = [k for k in pd.get_dummies(df[cols], prefix="", prefix_sep="").keys()]
+    for col1 in cols:
+        for i, col2 in enumerate(dictionary):
+            df.loc[df[col1] == col2, col1] = i
+    return df, dictionary
 
 
 if __name__ == "__main__":
@@ -36,14 +63,11 @@ if __name__ == "__main__":
         names=["petal_width", "petal_length", "sepal_width", "sepal_length", "class"])
     print(data.head())
     print(data.describe())
-
-    data = data.sort_values(by="petal_width")
+    
     data = float_to_one_hot(data, [col for col in data.columns if col != "class"], 3)
-
-    # dummy = pd.get_dummies(data)
-    # print(f"dummy : {dummy}")
-
-    print(data)
+    data, d = str_to_one_hot(data)
+    
+    print(data, d)
 
     x_cols = [col for col in data.columns if col != "class"]
     data_x = data.loc[:, x_cols]
